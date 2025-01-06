a96f7662337705997b71df64d763f00c5c1c4444
Daniel Levy <daniel.levy04@post.runi.ac.il>
2025-01-06 23:17:28 +0200

    6th commit

--- User.java
+++ User.java
@@ -53,7 +53,7 @@
     /** Makes this user follow the given name. If successful, returns true. 
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
-        if (this.follows(name.toLowerCase()) || this.fCount == 10) {
+        if (this.follows(name) || this.fCount == 10) {
             return false;
         }
         this.follows[this.fCount] = name.toLowerCase();
@@ -64,11 +64,11 @@
     /** Removes the given name from the follows list of this user. If successful, returns true.
      *  If the name is not in the list, does nothing and returns false. */
     public boolean removeFollowee(String name) {
-        if (this.fCount == 0 || !this.follows(name.toLowerCase())) {
+        if (this.fCount == 0 || !this.follows(name)) {
             return false;
         }
         int i=0;
-        while (!this.follows[i].equals(name.toLowerCase())) {
+        while (!this.follows[i].equals(name)) {
             i++;
         }
         for (int j=i; j<this.fCount-1; j++) {
